ac51769f1456f7664152e4a0f7c534bd6af2f6f5
Jacen Williams <jacencwilliams@gmail.com>
2021-08-25 11:21:24 -0500

    Add project files.

---
+++ BlazorWebViewOnWPF/Blazor/App.razor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorWebViewOnWPF.Blazor
+{
+    public partial class App
+    {
+    }
+}
---
+++ BlazorWebViewOnWPF/Services/WeatherDataService.cs
@@ -0,0 +1,21 @@
+﻿using BlazorWebViewOnWPF.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorWebViewOnWPF.Services
+{
+    class WeatherDataService
+    {
+        public async Task<IEnumerable<WeatherForecast>> GetWeatherForecastsAsync()
+        {
+            await Task.Delay(500);
+            var stringData = await File.ReadAllTextAsync("weather.json");
+            return JsonConvert.DeserializeObject<IEnumerable<WeatherForecast>>(stringData);
+        }
+    }
+}
